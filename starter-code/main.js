b183e118471766b2b5e2eeba7fc2472fe8240579
Misha LeClair <mishaleclair@gmail.com>
2016-07-24 21:54:16 -0700

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
+
+//define variables of the four cars//
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
