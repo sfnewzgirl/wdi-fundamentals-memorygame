cc5ff710af2d012650a3a9d25051e75032707e26
Misha LeClair <mishaleclair@gmail.com>
2016-07-27 15:12:37 -0700

    adding my html and css files

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,15 @@
-console.log("JS file is connected to HTML! Woo!")
-
 //define variables of the four cars//
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+
+//added game logic//
+if (cardOne === cardThree) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again!");
+}
+
+console.log(alert);
