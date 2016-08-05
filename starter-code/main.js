c7c6ec3c53c2868368398dd310c526410945023b
Misha LeClair <mishaleclair@gmail.com>
2016-08-04 21:53:04 -0700

    createBoard function and associated html changes

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,15 +1,30 @@
 //define variables of the four cars//
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-//added game logic Unit 9//
-if (cardOne === cardThree) {
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again!");
-}
+//var cardOne = 'queen';
+//var cardTwo = 'king';
+//var cardThree = 'queen';
+//var cardFour = 'king';
 
-console.log(alert);
+//added game logic//
+//if (cardOne === cardThree) {
+//	alert("You found a match!");
+//}
+//else {
+//	alert("Sorry, try again!");
+//}
+
+var cards = ['queen', 'king', 'queen', 'king'];
+
+var board = document.getElementById('game-board');
+
+console.log(board)
+
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('div');
+	cardElement.className = 'card';
+	board.appendChild(cardElement);
+	}
+};
+
+createBoard();
