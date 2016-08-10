3441f71aad6ffc55f4faa9ae246134318bab5076
Misha LeClair <mishaleclair@gmail.com>
2016-08-09 19:36:35 -0700

    final project with updated html, css, js and two playing card immages

--- starter-code/main.js
+++ starter-code/main.js
@@ -13,18 +13,45 @@
 //	alert("Sorry, try again!");
 //}
 
-var cards = ['queen', 'king', 'queen', 'king'];
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [ ];
 
 var board = document.getElementById('game-board');
 
-console.log(board)
-
-function createBoard () {
-	for (var i = 0; i < cards.length; i++) {
+function createBoard() {
+	for (var i = 0; i < cards.length; i++){
 	var cardElement = document.createElement('div');
 	cardElement.className = 'card';
+	cardElement.setAttribute('data-card', cards[i]);
 	board.appendChild(cardElement);
+	board.appendChild(cardElement);
+	cardElement.addEventListener('click', isTwoCards);
 	}
-};
+}
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') == 'king') {
+		this.innerHTML = '<img src="king card.jpg"/>';
+	}
+	else {
+		this.innerHTML = '<img src="queen card.jpg"/>';
+	}
+	if (cardsInPlay.length === 2) {
+	isMatch(cardsInPlay);
+	cardsInPlay = [];
+	}
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+	alert("You found a match!");
+	}
+	else {
+	alert("Sorry, try again!");
+	}
+}
 
 createBoard();
